4aa1026c21cdb8f370f22ad0ad27eb071dd09a23
zhangboyang <zhangboyang@zhangboangdeMBP.lan>
2019-11-03 16:43:56 +0800

    xx

--- app/models/user.js
+++ app/models/user.js
@@ -6,12 +6,23 @@ const userSchema = new Schema({
     __v: {type: Number, select: false},
     username: {type: String,required: true},
     password: {type: String,required: true,select:false},
-    sex: {type: String,enum:['male','feimale'],default:"male",required: true},
+    repassword: {type: String,required: true,select:false},
+    email: {type: String,required: false,select:false},
+    permission: {type: String,enum:['super','visitor'],required: true},
+    create_time: {
+        type: Date,
+        default: Date.now
+      },
+      update_time: {
+        type: Date,
+        default: Date.now
+      }
+    // sex: {type: String,enum:['male','feimale'],default:"male",required: true},
     // location: {type: [{type: String}]},
     // following: {
     //     type: [{ type: Schema.Types.ObjectId, ref: 'User' }],
     //     select: false,
     // }
-})
+},{timestamps:{createdAt: "create_time",updatedAt: "update_time"}});
 
 module.exports = model('User',userSchema)
